0351e341faf9b2e2c12c317a23000755b2805be5
vegopunk <241767368+dev-vegopunk@users.noreply.github.com>
2026-03-26 15:40:35 +0100

    redirect to dashboard if user logged in

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -49,6 +49,15 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
+  if (
+    user &&
+    request.nextUrl.pathname === '/'
+  ) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/dashboard'
+    return NextResponse.redirect(url)
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
